3083bfd7752aa249490c3830bf95a50bcb7c231b
Aurack <thomas.bonin06@gmail.com>
2024-06-04 22:43:58 +0200

    - make the resources and energy dependant of the map seed

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,8 @@ use crate::terrain::Terrain;
 
 pub fn get_char(val: f64) -> char {
     match val.abs() {
+        v if v > 0.00005 && v < 0.0001 => Terrain::Energy.to_char(),
+        v if v > 0.0001 && v < 0.0095 => Terrain::Ore.to_char(),
         v if v < 0.25 => Terrain::Ground.to_char(),
         v if v < 0.5 => Terrain::Wall.to_char(),
         _ => {Terrain::Mountain.to_char()}
